7889fb05c3239cf129e63ef8f969c51ca27cb3d6
edisi <edisiswanto.ti8.poliwangi@gmail.com>
2018-08-28 07:46:12 +0700

    Project terupdate dan sudah digunaka

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/log/';
+$config['base_url']	= 'http://localhost/gudang/';
 
 /*
 |--------------------------------------------------------------------------
